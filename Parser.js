9647a7d1bb26110239ccdccb361f22afd61a8081
luis rojas <lluisrojass@aol.com>
2017-01-29 22:52:06 -0500

    refactor

--- Parser.js
+++ Parser.js
@@ -1,12 +1,10 @@
 const htmlparser2 = require('htmlparser2');
 const ee = require('events');
-/*
-  TODO: Figure out reference to Request loop to change the pathname once found
-*/
 
 class ArchiveParser extends ee {
   constructor(postTypes){
     super();
+    const self = this;
     this._currMediaType = null;
     /* holds either 'is_photo' 'is_video' 'is_quote' 'is_regular'(text) 'is_chat' 'is_note'(question) 'is_audio'*/
     this._types =[];
@@ -15,8 +13,38 @@ class ArchiveParser extends ee {
     this._isMediaFound = !1;
     this._isDateFound = !1;
     this._date = '';
-    this._loop = null;
-    this._initParser();
+    // main parser
+    this._parser = new htmlparser2.Parser({
+      onopentag: function(name,attribs) {
+        if (name === "div" && attribs.class && self._validate(attribs.class)){
+          self._isMediaFound = !0;
+        }
+        else if (name === "a") {
+          if (self._isMediaFound && attribs['data-peepr'] ) {
+            const d = JSON.parse(attribs['data-peepr'])
+            self.emit('post',{'href':`www.${d.tumblelog}.tumblr.com/post/${d.postId}`,'type':self._currMediaType});
+            // clear variables
+            self._currMediaType = null;
+            self._isMediaFound = !1;
+          }
+          else if (attribs.id && attribs.id === "next_page_link") {
+            self.emit("nextPage",attribs.href); // subscribe to the change in the request loop
+          }
+        }
+        else if (name === "h2" && attribs.class && attribs.class === "date") {
+            self._isDateFound = !0;
+        }
+      },
+      ontext:function(t){
+        if (self._isDateFound){
+            if (t !== self._date){
+              self._date = t;
+              self.emit('date',self._date);
+            }
+            self._isDateFound = !1;
+        }
+      }
+    },{decodeEntities: true});
   }
   _validate(c) {
     var b = !1;
@@ -25,52 +53,40 @@ class ArchiveParser extends ee {
         if (c.includes(e)){
           b = !0;
           this._currMediaType = e;
-          console.log(e);
         }
       });
     }
     return b;
   };
   end(){ this._parser.end() }
-  /*addLoopRef(L){ this.loop = L; }*/
   write(chunk){ this._parser.write(chunk); }
-  _initParser(){
+}
+
+
+// For Parsing data from post pages
+class ContentParser extends ee{
+  constructor(){
     const self = this;
+    let found = !1;
     this._parser = new htmlparser2.Parser({
-      onopentag: function(name,attribs) {
-        if (name === "div" && attribs.class && self._validate(attribs.class)){
-          self._isMediaFound = !0;
-        }
-        else if (name === "a") {
-          if (self._isMediaFound) {
-            self.emit('post',{'host':self._loop._options.host,'path':attribs.href,'type':self._currMediaType});
-            // clear variables, maybe make method
-            self._currMediaType = null;
-            self._isMediaFound = !1;
-          }
-          else if (attribs.id && attribs.id === "next_page_link") {
-            //TODO: loose couple
-            /*self.loop._options.path = attribs.href;*/
-            self.emit("nextPage",/*self.loop._options.path*/ attribs.href);
-          }
-        }
-        else if (name === "h2" && attribs.class && attribs.class === "date") {
-            self._isDateFound = !0;
+      onopentag:function(name,attribs){
+        if(name === 'meta' && attribs.type && attribs.type === 'application/ld+json'){
+          found = !0;
         }
       },
       ontext:function(t){
-        if (self._isDateFound && self._date !== t){
-          self._date = t;
-          self.emit('date',self._date);
-          self._isDateFound = !1;
+        if(found){
+          self.emit('data',JSON.parse(t));
+          this._parser = null; // de-reference
+        } else {
+          return;
         }
       }
     },{decodeEntities: true});
   }
-}
-
-class ContentParser extends ee{
-
+  write(chunk){
+    this._parser.write(chunk)
+  }
 }
 
 module.exports.ArchiveParser = ArchiveParser;
--- PostData.js
+++ PostData.js
@@ -1,16 +1,18 @@
 const ee = require('events');
 const http = require('http');
-const htmlparser2 = require('htmlparser2');
+const pipeEvents = require('pipe-events');
+const ContentParser = require('./Parser').ContentParser;
 
 /* emits error(:param: message,:param: attempted URL path)*/
-module.exports = class postData extends ee {
+class postData extends ee {
   /*returns JSON object with data from post*/
   get(host,path,type){
-    const self = this;
+
     const returnData = {
       href:host+path,
       type:type
     };
+
     const options = {
       host:host,
       path:path+'/mobile',
@@ -19,29 +21,17 @@ module.exports = class postData extends ee {
         'user-agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1'
       }
     };
-    var isFound = !1;
-    const parser = new htmlparser2.Parser({
-      onopentag:function(name,attribs){
-        if (name === 'script' && attribs.type && attribs.type === 'application/ld+json'){ isFound = !0 }
-      },
-      ontext:function(text){
-        if (isFound) {
-          returnData.data = JSON.parse(text);
-          self.emit('postData',returnData);
-          isFound = !1;
-        }
-      }
-    },{decodeEntities: true});
 
-    http.get(options,(res)=>{
+    pipeEvents('data',self._parser,this);
+
+    http.get(options,(res){
       if (res.statusCode !== 200){
-        this.emit('error',`response code: ${statusCode}`,path)
-        return;
+        this.emit('error','status code '+res.statusCode);
       }
-      res.on('data',(chunk)=>parser.write(chunk));
-    }).on('error',()=>{
-      this.emit('error','unknown error with request',path)
-      return;
-    });
+      res.on('data'(chunk)=> this._parser.write(chunk));
+    }).on('error',()=>this.emit('requestError'));
+
+
   }
 }
+postData.prototype._parser = new ContentParser();
