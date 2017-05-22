3c7a9eb53a427885dbd2b93d00c710b41318a9a8
luis rojas <lluisrojass@aol.com>
2017-05-22 02:40:38 -0400

    github href link removed

--- components/renderer/react/notification.js
+++ components/renderer/react/notification.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 function Notification(props) {
-  const link = 'https://github.com/lluisrojass/Tumblr-Blog-Scraper';
   const type = ((t) => {
     switch(t){
       case 0:
@@ -15,13 +14,9 @@ function Notification(props) {
   return (
     <div id='notification-wrapper'>
       <p className={ 'notif-label vertical-center-contents ' + type }>
-        {
-          props.msg + ' '
-        }
-        <a href='#' onClick={ () => props.openInBrowser(link) }>
-           Click here to open issues or contribute to this project.
-        </a>
+        {props.msg + ' '}
       </p>
+
     </div>
   )
 }
