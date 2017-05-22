5bf7a9f2184bd458515bc2a9d6f147109ca1b6e8
luis rojas <lluisrojass@aol.com>
2017-05-22 02:42:56 -0400

    separated all text fields for posterity

--- components/renderer/react/viewer.js
+++ components/renderer/react/viewer.js
@@ -3,9 +3,9 @@ const date = require('datejs');
 import React from 'react';
 
 function Viewer(props){
-    if (!props.isViewing){
-      return(<div className='height100width100 notselected'> </div>)
-    }
+    if (!props.isViewing)
+      return(<div className='height100width100 notselected'> </div>);
+
     const {headline, datePublished, articleBody, images, url, video} = props.post;
     return(
       <div id='right-panel' className='height100width100'>
@@ -13,36 +13,45 @@ function Viewer(props){
             headline &&
               <h1 className='viewer-title'>
                 {headline}
-              </h1> ||
+              </h1>
+          }
+          {
             datePublished &&
               <h1 className='viewer-date'>
                 {Date.parse(datePublished).toString()}
-              </h1> ||
+              </h1>
+          }
+          {
             articleBody &&
-              <p className='viewer-body'>
-                {articleBody}
-              </p> ||
+            <p className='viewer-body'>
+              {articleBody}
+            </p>
+          }
+          {
             video &&
-              <div id='video-wrapper'>
-                <iframe frameBorder='0' src={video} />
+             <div id='video-wrapper'>
+              <iframe frameBorder='0' src={video} />
+             </div>
+          }
+          {
+            images.length > 0 &&
+            <div id='viewer-image-wrapper'>
+              {
+                 images.map((url, index) =>
+                   <img className='viewer-image' key={index} src={url} />
+                 )
+              }
+            </div>
+          }
+          <br/>
+          {
+            url &&
+             <div id='viewer-url-wrapper'>
+                    <a href='#' onClick={() => props.openInBrowser(url)} className='viewer-url'>
+                      Open in Browser..
+                    </a>
               </div>
           }
-          <div id='viewer-image-wrapper'>
-            {
-              images.map((imageUrl, index) =>
-                <img className='viewer-image' key={index} src={imageUrl} />
-              )
-            }
-          </div>
-          <br/>
-          <div id='viewer-url-wrapper'>
-            {
-              url &&
-                <a href='#' onClick={() => props.openInBrowser(url)} className='viewer-url'>
-                  Open in Browser..
-                </a>
-            }
-          </div>
       </div>
     )
 }
